77ed74a51fd91fa0806706acd5e0a88d51f977bb
XIOA <1327916255@qq.com>
2025-02-24 12:46:00 +0800

    Init

---
+++ SnackbarDemo/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Snackbar.Helper;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnackbarDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+        private void Test(object sender, RoutedEventArgs e)
+        {
+
+            SnackbarHelper.Show($"Test:{r.Next(10000,99999)}", 4000);
+        }
+    }
+}
